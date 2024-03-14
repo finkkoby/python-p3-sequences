10c78796e33452e8f5d5cc36ed847fbcf5b8712c
Koby Fink <finkkoby@icloud.com>
2024-03-14 09:29:13 -0400

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        answer = []
+    elif length < 3 and length > 0:
+        answer = []
+        for i in range(length):
+            answer.append(i)
+    else:
+        answer = [0, 1]
+        for i in range (2, length):
+            answer.append(answer[i-1] + answer[i-2])
+    print(answer)
